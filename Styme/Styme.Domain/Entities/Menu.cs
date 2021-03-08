b2a8459ffe6f13391784cc1492caffb485b496bc
tuliopaim <tutpaim@gmail.com>
2021-03-08 18:01:45 -0300

    Adiciona entidades

---
+++ Styme/Styme.Domain/Entities/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Styme.Domain.Entities
+{
+    public class Menu : Entity
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Uri ImageUri { get; set; }
+        public string Category { get; set; }
+
+        public long RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+    }
+}
---
+++ Styme/Styme.Domain/Entities/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Styme.Domain.Entities
+{
+    public class Restaurant : Entity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Category { get; set; }
+        public string Location { get; set; }
+        public Uri ImageUri { get; set; }
+
+        public IEnumerable<Menu> Menus { get; set; }
+    }
+}
