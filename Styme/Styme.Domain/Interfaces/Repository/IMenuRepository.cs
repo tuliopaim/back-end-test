b2071da5b645cc0b6f0860b35ac06ff707639113
tuliopaim <tutpaim@gmail.com>
2021-03-08 19:16:30 -0300

    Adiciona interfaces de repositorio

---
+++ Styme/Styme.Domain/Interfaces/Repository/IMenuRepository.cs
@@ -0,0 +1,22 @@
+﻿using Styme.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Styme.Domain.Interfaces.Repository
+{
+    public interface IMenuRepository
+    {
+        void Insert(Menu menu);
+
+        void Update(Menu menu);
+
+        void Delete(Menu menu);
+
+        IList<Menu> Select();
+
+        Menu SelectById(long id);
+    }
+}
---
+++ Styme/Styme.Domain/Interfaces/Repository/IRestaurantRepository.cs
@@ -0,0 +1,22 @@
+﻿using Styme.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Styme.Domain.Interfaces.Repository
+{
+    public interface IRestaurantRepository
+    {
+        void Insert(Restaurant restaurant);
+
+        void Update(Restaurant restaurant);
+
+        void Delete(Restaurant restaurant);
+
+        IList<Restaurant> Select();
+
+        Restaurant SelectById(long id);
+    }
+}
