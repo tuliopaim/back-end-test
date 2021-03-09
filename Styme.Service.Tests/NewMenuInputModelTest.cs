85f95ea16d9356d3fffef755d06854505b8fd388
tuliopaim <tutpaim@gmail.com>
2021-03-09 10:19:36 -0300

    Adiciona testes de NewRestaurante e NewInput

---
+++ Styme.Service.Tests/NewMenuInputModelTest.cs
@@ -0,0 +1,76 @@
+﻿using Styme.Service.Models.InputModels;
+using Xunit;
+
+namespace Styme.Service.Tests
+{
+    public class NewMenuInputModelTest
+    {
+        [Fact]
+        public void DeveSerInvalidoComDescriptionVazioOuNulo()
+        {
+            var input = new NewMenuInputModel
+            {
+                Price = 10.99m,
+                Category = "Categoria 01",
+                RestaurantId = 1
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerInvalidoComCategoryVazioOuNulo()
+        {
+            var input = new NewMenuInputModel
+            {
+                Description = "Menu 01",
+                Price = 10.99m,
+                RestaurantId = 1
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerInvalidoComPriceZero()
+        {
+            var input = new NewMenuInputModel
+            {
+                Description = "Menu 01",
+                Category = "Categoria 01",
+                Price = 0m,
+                RestaurantId = 1
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerInvalidoComRestauranteIdInvalido()
+        {
+            var input = new NewMenuInputModel
+            {
+                Description = "Menu 01",
+                Category = "Categoria 01",
+                Price = 0m,
+                RestaurantId = 0
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerValidoComInformacoesPreenchidas()
+        {
+            var input = new NewMenuInputModel
+            {
+                Description = "Menu 01",
+                Category = "Categoria 01",
+                Price = 10m,
+                RestaurantId = 1
+            };
+
+            Assert.True(input.IsValid);
+        }
+    }
+}
---
+++ Styme.Service.Tests/NewRestaurantInputModelTest.cs
@@ -0,0 +1,75 @@
+using Styme.Service.Models.InputModels;
+using System;
+using Xunit;
+
+namespace Styme.Service.Tests
+{
+    public class NewRestaurantInputModelTest
+    {
+        [Fact]
+        public void DeveSerInvalidoComNameVazioOuNulo()
+        {
+            var input = new NewRestaurantInputModel
+            {
+                Address = "Av. Araxá",
+                Category = "Restaurante",
+                Location = "Goiânia"
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerInvalidoComAddressVazioOuNulo()
+        {
+            var input = new NewRestaurantInputModel
+            {
+                Name = "Restaurante",
+                Category = "Restaurante",
+                Location = "Goiânia"
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerInvalidoComCategoryVazioOuNulo()
+        {
+            var input = new NewRestaurantInputModel
+            {
+                Name = "Restaurante",
+                Address = "Av. Araxá",
+                Location = "Goiânia"
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerInvalidoComLocationVazioOuNulo()
+        {
+            var input = new NewRestaurantInputModel
+            {
+                Name = "Restaurante",
+                Category = "Restaurante",
+                Address = "Av. Araxá",
+            };
+
+            Assert.False(input.IsValid);
+        }
+
+        [Fact]
+        public void DeveSerValidoComInformacoesPreenchidas()
+        {
+            var input = new NewRestaurantInputModel
+            {
+                Name = "Restaurante",
+                Category = "Restaurante",
+                Address = "Av. Araxá",
+                Location = "Goiânia"
+            };
+
+            Assert.True(input.IsValid);
+        }
+    }
+}
