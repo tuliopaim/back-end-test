4d03eecd1b1353356b42084680f0a316eb7f4ca6
tuliopaim <tutpaim@gmail.com>
2021-03-09 00:19:33 -0300

    Altera ServiceResultModel para ServiceResult, implementa mais facilitadores

---
+++ src/Styme.Service/Models/ServiceResult.cs
@@ -0,0 +1,65 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Styme.Service.Models
+{
+    public class ServiceResult
+    {
+        private ServiceResult()
+        {
+        }
+
+        public ServiceResult(ValidationResult results)
+        {
+            Success = results.IsValid;
+
+            Errors = results.Errors?.Select(e => $"[{e.PropertyName}] - {e.ErrorMessage}").ToList();
+        }
+
+        public ServiceResult(Exception exception)
+        {
+            Success = false;
+            Message = $"Ocorreu um erro: {exception.Message}";
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public object Data { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string Message { get; set; }
+
+        public bool Success { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<string> Errors { get; set; }
+
+        public static ServiceResult SuccessResult (object data = null, string message = null)
+        {
+            return new ServiceResult
+            {
+                Data = data,
+                Message = message,
+                Success = true
+            };
+        }
+
+        public static ServiceResult ErrorResult(string message = null, List<string> errors = null)
+        {
+            errors ??= !string.IsNullOrWhiteSpace(message)
+                ? new List<string> { message }
+                : new List<string>();
+
+            return new ServiceResult
+            {
+                Message = message,
+                Success = false,
+                Errors = errors
+            };
+        }
+    }
+}
