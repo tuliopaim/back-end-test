58252d4ef331ed6320ab7032e24a514779854df7
tuliopaim <tutpaim@gmail.com>
2021-03-09 00:18:45 -0300

    Altera DIConfiguration para ApiConfiguration e registra Mappers

---
+++ src/Styme.API/Configuration/ApiConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Styme.Data.Repository;
+using Styme.Domain.Interfaces.Repository;
+using Styme.Service.Interfaces;
+using Styme.Service.Services;
+using Styme.Service.Models;
+using Styme.Service.Models.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Styme.Domain.Entities;
+
+namespace Styme.API.Configuration
+{
+    public static class ApiConfiguration
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRestaurantService, RestaurantService>(); 
+
+            services.AddScoped<IRestaurantRepository, RestaurantRepository>();
+            services.AddScoped<IMenuRepository, MenuRepository>();
+
+            services.RegisterMappers();
+        }
+
+        public static void RegisterMappers(this IServiceCollection services)
+        {
+            services.AddSingleton(new MapperConfiguration(config =>
+            {
+                config.CreateMap<NewRestaurantInputModel, Restaurant>();
+                config.CreateMap<UpdateRestaurantInputModel, Restaurant>();
+                config.CreateMap<NewMenuInputModel, Menu>();
+            }).CreateMapper());
+        }
+    }
+}
