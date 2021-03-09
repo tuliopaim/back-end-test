a6eeb23d8c17decce41c1c9399fed116155a7402
tuliopaim <tutpaim@gmail.com>
2021-03-08 23:00:57 -0300

    Implementa classe ServiceResultModel

--- src/Styme.Service/Models/ServiceResultModel.cs
+++ src/Styme.Service/Models/ServiceResultModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,47 @@ namespace Styme.Service.Models
 {
     public class ServiceResultModel
     {
-        public ServiceResultModel()
+        private ServiceResultModel()
         {
+        }
 
+        public ServiceResultModel(ValidationResult results)
+        {
+            Valid = results.IsValid;
+
+            Errors = results.Errors?.Select(e => $"[{e.PropertyName}] - {e.ErrorMessage}").ToList();
+        }
+
+        public object Data { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Valid { get; set; }
+
+        public List<string> Errors { get; set; }
+
+        public static ServiceResultModel Success (object data = null, string message = null)
+        {
+            return new ServiceResultModel
+            {
+                Data = data,
+                Message = message,
+                Valid = true
+            };
+        }
+
+        public static ServiceResultModel Error(string message = null, List<string> errors = null)
+        {
+            errors ??= !string.IsNullOrWhiteSpace(message)
+                ? new List<string> { message }
+                : new List<string>();
+
+            return new ServiceResultModel
+            {
+                Message = message,
+                Valid = false,
+                Errors = errors
+            };
         }
     }
 }
