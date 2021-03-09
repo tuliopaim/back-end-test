ac914d98fbe2cbd8cee2acabe5662a02c2300e00
tuliopaim <tutpaim@gmail.com>
2021-03-09 12:18:06 -0300

    Cria filtro paginado e retorno paginado

---
+++ src/Styme.Domain/Filters/PaginatedFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Styme.Domain.Filters
+{
+    public class PaginatedFilter
+    {
+        private int _page;
+
+        public int Page
+        {
+            get => _page;
+
+            set => _page = value == 0 ? 1 : value;
+        }
+
+        public string SearchTerm { get; set; }
+
+        public int PageSize { get; set; }
+
+        public bool OrderByDesc { get; set; }
+
+        public int StartIndex => (Page - 1) * PageSize;
+    }
+}
